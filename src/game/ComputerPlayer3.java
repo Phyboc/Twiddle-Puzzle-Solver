45a73a7e9b6b259c407842e524988874710f3b30
Phyboc <sivasubramanikj@gmail.com>
2026-03-08 16:41:53 +0530

    Modified computerplayer3 to make it a top-down kind of approach inspired by mdf

--- src/game/ComputerPlayer3.java
+++ src/game/ComputerPlayer3.java
@@ -3,62 +3,124 @@ package game;
 import java.util.*;
 
 public class ComputerPlayer3 extends AbstractPlayer {
-	private class MDP{
-		int grid[][];
-		int cost;
-		public MDP(int[][] grid, int cost) {
-			this.grid = grid;
-			this.cost = cost;
-		}
-		public MDP(int[][] grid) {
-			this.grid = grid;
-			this.cost = Integer.MAX_VALUE;
-		}
-		
-	}
-	private int finalgrid[][];
-	private ArrayList<MDP> dp;
-	public ComputerPlayer3(Board board) {
-		super(board);
-		this.finalgrid = new int[board.size()][board.size()];
-		this.dp = new ArrayList<>();
-        int val = 1;
-        for (int i = 0; i < finalgrid.length; i++)
-            for (int j = 0; j < finalgrid[0].length; j++)
-                finalgrid[i][j] = val++;
-		// TODO Auto-generated constructor stub
-	}
-	
-	@Override
-	public int getMove() {
-		// TODO Auto-generated method stub
-		int mvno = solvehdp();
-		return 0;
-	}
 
-	@Override
-	public String getName() {
-		// TODO Auto-generated method stub
-		return "Heuristic Dynamic Programming";
-	}
-	
-	private int solvehdp() {
-		MDP mdp = new MDP(board.getGrid());
-		dp.add(mdp);
-		int initial_cost = getcost(mdp);
-	}
-	private int getcost(MDP mdp) {
-		int grid[][] = mdp.grid;
-		int cost = mdp.cost;
-	}
-	
-	private String encode(int[][] g){
-        StringBuilder sb=new StringBuilder();
-        for(int[] r:g)
-            for(int x:r)
-                sb.append(x).append(',');
-        return sb.toString();
+    private Map<String, Integer> valueTable = new HashMap<>();
+    private boolean tableBuilt = false;
+
+    public ComputerPlayer3(Board board) {
+        super(board);
     }
 
+    @Override
+    public String getName() {
+        return "DP Solver (Reverse BFS)";
+    }
 
+    @Override
+    public int getMove() {
+
+        if (!tableBuilt) {
+            buildValueTable();
+            tableBuilt = true;
+            System.out.println("DP table built. States stored: " + valueTable.size());
+        }
+
+        int[][] grid = board.getGrid();
+        int bestMove = 1;
+        int bestValue = Integer.MAX_VALUE;
+
+        for (int m = 1; m <= board.totalMoves(); m++) {
+
+            int[][] next = rotateCopy(grid, m);
+            String key = encode(next);
+
+            Integer val = valueTable.get(key);
+
+            if (val != null && val < bestValue) {
+                bestValue = val;
+                bestMove = m;
+            }
+        }
+
+        return bestMove;
+    }
+
+    // =============================
+    // BUILD FULL DP TABLE
+    // =============================
+
+    private void buildValueTable() {
+
+        int N = board.getGrid().length;
+
+        int[][] goal = new int[N][N];
+        int v = 1;
+
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                goal[i][j] = v++;
+
+        Queue<int[][]> q = new ArrayDeque<>();
+
+        q.add(goal);
+        valueTable.put(encode(goal), 0);
+
+        while (!q.isEmpty()) {
+
+            int[][] state = q.poll();
+            int dist = valueTable.get(encode(state));
+
+            for (int m = 1; m <= board.totalMoves(); m++) {
+
+                int[][] next = rotateCopy(state, m);
+                String key = encode(next);
+
+                if (!valueTable.containsKey(key)) {
+
+                    valueTable.put(key, dist + 1);
+                    q.add(next);
+                }
+            }
+        }
+    }
+
+    // =============================
+    // ROTATION
+    // =============================
+
+    private int[][] rotateCopy(int[][] src, int move) {
+
+        int N = src.length;
+
+        int[][] g = new int[N][N];
+
+        for (int i = 0; i < N; i++)
+            g[i] = src[i].clone();
+
+        int r = (move - 1) / (N - 1);
+        int c = (move - 1) % (N - 1);
+
+        int tmp = g[r][c];
+        g[r][c] = g[r][c + 1];
+        g[r][c + 1] = g[r + 1][c + 1];
+        g[r + 1][c + 1] = g[r + 1][c];
+        g[r + 1][c] = tmp;
+
+        return g;
+    }
+
+    // =============================
+    // ENCODE STATE
+    // =============================
+
+    private String encode(int[][] g) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int[] r : g)
+            for (int x : r)
+                sb.append(x).append(',');
+
+        return sb.toString();
+    }
 }
